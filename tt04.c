f3a290f99dc18a81862517def3d3291af8db02a1
subin073 <144412727+subin073@users.noreply.github.com>
2023-11-25 20:32:02 +0900

    Create tt04.c

---
+++ tt04.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main() {
+    int per[4] = { 25, 38, 0, 40 }; // 초기값 설정
+    int n, div, stu;
+
+    printf("=== 수강신청 관리 프로그램 ===");
+
+    while (1) {
+        printf("\n");
+        printf("현재 분반별 C프로그래밍1 수강 인원은 다음과 같습니다.(정원: 40명)\n");
+
+        for (int i = 0; i < 4; i++) {
+            printf("%d분반 %d명 ", i + 1, per[i]);
+        }
+        printf("\n");
+
+        printf("명령을 입력하세요.(1.수강인원 추가, 2.수강인원 감소, 기타.종료)> ");
+        scanf_s("%d", &n);
+
+
+        switch (n) {
+
+        case 1:
+            printf("분반을 선택하세요 (1~4)> ");
+            scanf_s("%d", &div);
+            div--; // 분반이랑 인덱스 같게
+
+            if (div >= 0 && div <= 3)
+            {
+                printf("추가할 수강인원을 입력하세요: ");
+                scanf_s("%d", &stu);
+
+                if (per[div] + stu <= 40) {
+                    per[div] += stu;
+                    printf("%d분반의 수강인원이 %d명 추가되었습니다.\n", div + 1, stu);
+                }
+                else {
+                    printf("%d분반은 수강인원 초과입니다.\n", div + 1);
+                }
+            }
+            else {
+                printf("잘못된 분반 선택입니다.\n");
+            }
+            break;
+
+        case 2:
+            printf("분반을 선택하세요 (1~4)> ");
+            scanf_s("%d", &div);
+            div--;
+
+
+                if (div >= 0 && div <= 3)
+                {
+                    printf("감소할 수강인원을 입력하세요: ");
+                    scanf_s("%d", &stu);
+
+                    if (per[div] - stu >= 0) {
+                        per[div] -= stu;
+                        printf("%d분반의 수강인원이 %d명 감소되었습니다.\n", div + 1, stu);
+                    }
+                    else {
+                        printf("%d분반은 수강인원이 없습니다.\n", div + 1);
+                    }
+                }
+                else {
+                    printf("잘못된 분반 선택입니다.\n");
+                }
+            break;
+
+           if (n != 1 && n != 2)
+            {
+                printf("프로그램을 종료합니다.\n");
+                break;
+            }
+        }
+    }
+    return 0;
+}
